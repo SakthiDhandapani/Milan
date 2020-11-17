8d374b8ff7bd564292bc430bb36a3e62483088a9
SakthiDhandapani <sakthi.dhandapani@gmail.com>
2020-11-17 13:50:39 +0530

    Update LambdaFunctionHandler.java

--- src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
+++ src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
@@ -8,9 +8,29 @@ public class LambdaFunctionHandler implements RequestHandler<Object, String> {
     @Override
     public String handleRequest(Object input, Context context) {
         context.getLogger().log("Input: " + input);
-
-        // TODO: implement your handler
-        return "Hello ==MILAN===T>>>>>>>CICID TESTING ON MONDAY Milan Tested Demo !!!!<<<<<<S==> Lambda!";
+        int sum = callAddition();
+        int sub = callSub();
+        int mul = callMul();
+        context.getLogger().log("Addition: " + sum);
+        context.getLogger().log("Subtraction: " + sub);
+        context.getLogger().log("Multiplication: " + mul);
+        return "Hello"+input+"Welcome";
+    }
+    public int callAddition(){
+        int numOne = 5;
+        int numTwo = 10;
+        return numOne+numTwo;
     }
 
+        public int callSub(){
+        int numOne = 5;
+        int numTwo = 10;
+        return numTwo - numTwo;
+    }
+
+        public int callMul(){
+        int numOne = 5;
+        int numTwo = 10;
+        return numOne*numTwo;
+    }
 }
