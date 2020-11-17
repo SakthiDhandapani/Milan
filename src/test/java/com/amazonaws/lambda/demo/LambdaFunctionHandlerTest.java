3c91c5fa769d2029bfae29a18f2fbef2ded23076
SakthiDhandapani <sakthi.dhandapani@gmail.com>
2020-11-17 13:35:02 +0530

    Add files via upload

---
+++ src/test/java/com/amazonaws/lambda/demo/LambdaFunctionHandlerTest.java
@@ -0,0 +1,58 @@
+package com.amazonaws.lambda.demo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+public class LambdaFunctionHandlerTest {
+	@Test
+	public void testLambdaFunctionHandler() {
+		LambdaFunctionHandler handler = new LambdaFunctionHandler();
+		Object input = "Milan";
+		Context ctx = createContext();
+		Object output = handler.handleRequest(input, ctx);
+		if (output != null) {
+			System.out.println(output.toString());
+		}
+		Assert.assertEquals("Hello" + input + "Welcome", output);
+	}
+
+	private Context createContext() {
+		TestContext ctx = new TestContext();
+		ctx.setFunctionName("milanfunction");
+
+		return ctx;
+	}
+
+	@Test
+	public void testLambdaFunctionHandlerSum() {
+		LambdaFunctionHandler handler = new LambdaFunctionHandler();
+		Object output = handler.callAddition();
+		if (output != null) {
+			System.out.println(output.toString());
+		}
+		Assert.assertEquals(15, output);
+	}
+
+	@Test
+	public void testLambdaFunctionHandlerSub() {
+		LambdaFunctionHandler handler = new LambdaFunctionHandler();
+		Object output = handler.callSub();
+		if (output != null) {
+			System.out.println(output.toString());
+		}
+		Assert.assertEquals(5, output);
+	}
+
+	@Test
+	public void testLambdaFunctionHandlerMul() {
+		LambdaFunctionHandler handler = new LambdaFunctionHandler();
+		Object output = handler.callMul();
+		if (output != null) {
+			System.out.println(output.toString());
+		}
+		Assert.assertEquals(50, output);
+	}
+
+}
